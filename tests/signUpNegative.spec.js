9a68c341a194b9251d1e12928e71b334d9903c50
RuslanKravchuk2907 <meladzeruslan164@gmail.com>
2026-03-31 17:43:00 +0300

    sign up negative test

--- tests/signUpNegative.spec.js
+++ tests/signUpNegative.spec.js
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Assert error message for empty username in Sign up form', async ({ page }) => {
+    await page.goto('https://conduit.mate.academy/user/register');
+    await page.getByPlaceholder('Email').fill('kravchukqaruslan@gmail.com');
+    await page.getByPlaceholder('Password').fill('123456');
+    await page.getByRole('button',{name:'Sign up'}).click();
+    expect(page.getByRole('list').nth(1)).toContainText('Username must start with a letter, have no spaces, and be 2 - 40 characters.');
 
 });
 
